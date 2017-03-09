ba8aa87bdaf8538e145e45b5867ca2d229f947bd
William Villeneuve <ciiqr@users.noreply.github.com>
2017-03-08 21:20:52 -0500

    Set git email as specified in private config

--- development/install.sh
+++ development/install.sh
@@ -3,7 +3,7 @@
 # Git
 $DEBUG git config --global push.default simple
 $DEBUG git config --global user.name "William Villeneuve"
-# TODO: $DEBUG git config --global user.email "user@example.com"
+
 $DEBUG git config --global alias.cm '!git commit -m'
 $DEBUG git config --global alias.st status
 $DEBUG git config --global alias.co checkout
@@ -12,4 +12,8 @@ $DEBUG git config --global alias.br branch
 if contains_option frontend "${categories[@]}"; then
 	$DEBUG git config --global diff.tool meld
 	$DEBUG git config --global difftool.prompt false
+fi
+
+if [[ ${priv_conf[@]+isset} ]]; then
+	$DEBUG git config --global user.email "${priv_conf[git_email]}"
 fi
