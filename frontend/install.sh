d565ba754958b613769cb1d021c05ddb205dd08a
William Villeneuve <ciiqr@users.noreply.github.com>
2017-03-10 00:28:07 -0500

    Fix the permissions of the ssh keys after they've been transfered...

--- frontend/install.sh
+++ frontend/install.sh
@@ -6,4 +6,7 @@ make_directory ssh_backup "$home_backup/.ssh"
 if [[ ${priv_conf[@]+isset} ]]; then
 	# Transfer private ssh configs
 	transfer "${priv_conf[ssh_configs]}" "$destination/.ssh" "$ssh_backup"
+
+	# Fix permissions of keys
+	$DEBUG chmod 0600 "$destination/.ssh/keys/"*
 fi
