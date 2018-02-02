7294eec17da579c0a4e6534da7bc7be882f00b03
William Villeneuve <ciiqr@users.noreply.github.com>
2018-02-01 22:35:33 -0500

    added cli arguments, fixed salt install for osx

--- readme.md
+++ readme.md
@@ -5,11 +5,11 @@
 * remote
 
 ```
-curl -sL https://raw.githubusercontent.com/ciiqr/config/master/scripts/install | sudo bash
+curl -sL https://raw.githubusercontent.com/ciiqr/config/master/scripts/install | sudo bash -s -- --roles "frontend sublime development"
 ```
 
 * local (change path to wherever you've cloned to)
 
 ```
-~/Dropbox/Projects/config/scripts/install
+~/Dropbox/Projects/config/scripts/install --roles "frontend sublime development"
 ```
--- scripts/include/common.sh
+++ scripts/include/common.sh
@@ -1,6 +1,55 @@
-function ensureRoot() {
+ensureRoot()
+{
 	if [[ "$EUID" -ne 0 ]]; then
 	   echo "must be run as root" 1>&2
 	   exit 1
 	fi
+}
+
+set_cli_args_default()
+{
+    force="false"
+    link="true"
+    configDir="/config"
+    privateConfigDir="/config-private"
+    saltDir="/etc/salt"
+    roles=""
+}
+
+parse_cli_args()
+{
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            --link)
+                link="true"
+            ;;
+            --copy)
+                link="false"
+            ;;
+            --configDir)
+                configDir="${2%/}"
+                shift
+            ;;
+            --privateConfigDir)
+                privateConfigDir="${2%/}"
+                shift
+            ;;
+            --saltDir)
+                saltDir="${2%/}"
+                shift
+            ;;
+            --roles)
+                roles="$2"
+                shift
+            ;;
+            --force)
+                force="true"
+            ;;
+            *)
+                echo "$0: Unrecognized option $1" 1>&2
+                return 1
+            ;;
+        esac
+        shift
+    done
 }
