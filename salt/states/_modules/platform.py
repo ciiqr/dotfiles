57a8822b18a8acd4eb52217ba727122ffb574396
William Villeneuve <ciiqr@users.noreply.github.com>
2018-02-02 22:50:06 -0500

    fixed indentation

--- salt/states/_modules/platform.py
+++ salt/states/_modules/platform.py
@@ -6,10 +6,10 @@ except ImportError:
     import salt.utils as _platform
 
 def get_name(**kwargs):
-	if _platform.is_windows():
-		return 'windows'
-	elif _platform.is_darwin():
-		return 'osx'
-	elif _platform.is_linux():
-		return 'linux'
-	return ''
+    if _platform.is_windows():
+        return 'windows'
+    elif _platform.is_darwin():
+        return 'osx'
+    elif _platform.is_linux():
+        return 'linux'
+    return ''
--- salt/states/_modules/rootscheck.py
+++ salt/states/_modules/rootscheck.py
@@ -4,30 +4,30 @@ import logging
 log = logging.getLogger(__name__)
 
 def state_exists(*args, **kwargs):
-	return any(True for name in args if _exists('file_roots', name, **kwargs))
+    return any(True for name in args if _exists('file_roots', name, **kwargs))
 
 def pillar_exists(*args, **kwargs):
-	return any(True for name in args if _exists('pillar_roots', name, **kwargs))
+    return any(True for name in args if _exists('pillar_roots', name, **kwargs))
 
 def _exists(type, name, **kwargs):
-	env = __opts__.get('saltenv') or 'base'
-	if env not in __opts__[type]:
-		log.warning('saltenv "{0}": does not exist'.format(env))
-		return False
+    env = __opts__.get('saltenv') or 'base'
+    if env not in __opts__[type]:
+        log.warning('saltenv "{0}": does not exist'.format(env))
+        return False
 
-	slash_name = name.replace('.', '/')
+    slash_name = name.replace('.', '/')
 
-	# prefix relative path's with sls path
-	if slash_name and slash_name[0] == '/':
-		slspath = kwargs.get('slspath', '')
-		slash_name = slspath + slash_name
+    # prefix relative path's with sls path
+    if slash_name and slash_name[0] == '/':
+        slspath = kwargs.get('slspath', '')
+        slash_name = slspath + slash_name
 
-	for root in __opts__[type][env]:
-		base_path = '{0}/{1}'.format(root, slash_name)
-		init_path = '{0}/init.sls'.format(base_path, slash_name)
-		sls_path = '{0}.sls'.format(base_path, slash_name)
+    for root in __opts__[type][env]:
+        base_path = '{0}/{1}'.format(root, slash_name)
+        init_path = '{0}/init.sls'.format(base_path, slash_name)
+        sls_path = '{0}.sls'.format(base_path, slash_name)
 
-		if any(__salt__['file.file_exists'](path) for path in [init_path, sls_path]):
-			return True
+        if any(__salt__['file.file_exists'](path) for path in [init_path, sls_path]):
+            return True
 
-	return False
+    return False
