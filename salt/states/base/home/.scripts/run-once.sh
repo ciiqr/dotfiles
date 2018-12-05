8aecc94c7db353dd818794b354685fe5fe697e4e
William Villeneuve <ciiqr@users.noreply.github.com>
2018-12-05 13:04:15 -0500

    added run-once script

---
+++ salt/states/base/home/.scripts/run-once.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+main()
+{
+    declare program
+    declare user="$USER"
+
+    # NOTES:
+    # - sudo indicates running as root (currently only supports current user or root)
+    # - first non-sudo part is assumed to be the program
+    for part in $@; do
+        if [[ "$part" == "sudo" ]]; then
+            user=root
+        else
+            program="$part"
+            break
+        fi
+    done
+
+    # run if not already running
+    is-running "$program" "$user" || "$@"
+}
+
+main "$@"
