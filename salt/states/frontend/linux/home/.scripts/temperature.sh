660f7f7e1e1a3392763f0d0395356f0a2a4eb81b
William Villeneuve <ciiqr@users.noreply.github.com>
2018-12-05 11:48:44 -0500

    some new scripts

---
+++ salt/states/frontend/linux/home/.scripts/temperature.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+temperature::get()
+{
+    sensors 2>/dev/null | sed -n 's/.*:\s*+\([0-9]*\).*/\1/p' | head -1
+}
+
+main()
+{
+    case "$1" in
+        get)
+            temperature::get
+            ;;
+        *)
+            echo 'usage: '
+            echo '  ~/.scripts/temperature.sh get'
+            ;;
+    esac
+}
+
+main "$@"
---
+++ salt/states/frontend/x11-home/.scripts/debug-awesome.sh
@@ -0,0 +1,2 @@
+Xephyr -ac -br -noreset -screen 1920x1080 -screen 1920x1080 +xinerama :1 &
+DISPLAY=:1.0 awesome
