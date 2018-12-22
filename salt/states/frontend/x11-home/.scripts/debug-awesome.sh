4070f1bf56bf49639f61a804c17b4429382027fc
William Villeneuve <ciiqr@users.noreply.github.com>
2018-12-22 15:07:49 -0500

    make ~/.xinitrc run for Xephyr

--- salt/states/frontend/x11-home/.scripts/debug-awesome.sh
+++ salt/states/frontend/x11-home/.scripts/debug-awesome.sh
@@ -1,2 +1,2 @@
-Xephyr -ac -br -noreset -screen 1920x1080 -screen 1920x1080 +xinerama +extension RANDR :1 &
+startx -- /usr/bin/Xephyr -ac -br -noreset -screen 1920x1080 -screen 1920x1080 +xinerama +extension RANDR :1 &
 DISPLAY=:1.0 awesome
