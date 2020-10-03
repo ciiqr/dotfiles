c2de391e717af733e796cca2ea269ebd6101e43f
William Villeneuve <ciiqr@users.noreply.github.com>
2020-10-03 00:58:29 -0400

    only backup retail wow, not playing classic anyways

--- salt/states/base/home/.scripts/backup.sh
+++ salt/states/base/home/.scripts/backup.sh
@@ -243,7 +243,7 @@ backup::_prepare_backup_paths()
         paths+=(/private/etc)
     elif ~/.scripts/system.sh is-windows; then
         paths+=(~/Documents)
-        backup::_append_existent_paths '/c/Program Files (x86)/World of Warcraft/_'{retail,classic}'_'/{Interface/Addons,WTF}
+        backup::_append_existent_paths '/c/Program Files (x86)/World of Warcraft/_retail_'/{Interface/Addons,WTF}
     fi
 
     # base
