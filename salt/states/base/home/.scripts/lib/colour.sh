75e0ab13fc0562bfdb2e383aedb21c0abd4cef58
William Villeneuve <ciiqr@users.noreply.github.com>
2021-06-11 11:56:05 -0400

    added colour:: shell functions

---
+++ salt/states/base/home/.scripts/lib/colour.sh
@@ -0,0 +1,74 @@
+colour::fg::black()
+{
+    tput setaf 0
+}
+colour::fg::red()
+{
+    tput setaf 1
+}
+colour::fg::green()
+{
+    tput setaf 2
+}
+colour::fg::yellow()
+{
+    tput setaf 3
+}
+colour::fg::blue()
+{
+    tput setaf 4
+}
+colour::fg::magenta()
+{
+    tput setaf 5
+}
+colour::fg::cyan()
+{
+    tput setaf 6
+}
+colour::fg::white()
+{
+    tput setaf 7
+}
+
+colour::bg::black()
+{
+    tput setab 0
+}
+colour::bg::red()
+{
+    tput setab 1
+}
+colour::bg::green()
+{
+    tput setab 2
+}
+colour::bg::yellow()
+{
+    tput setab 3
+}
+colour::bg::blue()
+{
+    tput setab 4
+}
+colour::bg::magenta()
+{
+    tput setab 5
+}
+colour::bg::cyan()
+{
+    tput setab 6
+}
+colour::bg::white()
+{
+    tput setab 7
+}
+
+colour::reset()
+{
+    tput sgr0
+}
+colour::bold()
+{
+    tput bold
+}
