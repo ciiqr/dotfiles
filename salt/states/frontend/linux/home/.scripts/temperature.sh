ac3e1d1790eac4fba1502f36a2f60457475eacb4
William Villeneuve <ciiqr@users.noreply.github.com>
2021-12-23 01:18:16 -0500

    fixed temperature script

--- salt/states/frontend/linux/home/.scripts/temperature.sh
+++ salt/states/frontend/linux/home/.scripts/temperature.sh
@@ -2,7 +2,9 @@
 
 temperature::get()
 {
+    # TODO: make this smarter
     sensors coretemp-isa-0000 2>/dev/null | sed -n 's/.*:\s*+\([0-9]*\).*/\1/p' | head -1
+    sensors dell_smm-isa-0000 2>/dev/null | sed -n 's/.*:\s*+\([0-9]*\).*/\1/p' | head -1
 }
 
 main()
