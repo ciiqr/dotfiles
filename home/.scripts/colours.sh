2c2889346daefbb7adc78785364b143cb7719ec2
William Villeneuve <ciiqr@users.noreply.github.com>
2022-10-07 21:23:23 -0400

    replaced colour scripts with new improved one

--- home/.scripts/colours.sh
+++ home/.scripts/colours.sh
@@ -1,51 +1,38 @@
 #!/usr/bin/env bash
 
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar. See
-# http://sam.zoy.org/wtfpl/COPYING for more details.
+set -e
 
-#Background
-for clbg in {40..47} {100..107} 49; do
-    #Foreground
-    for clfg in {30..37} {90..97} 39; do
-        #Formatting
-        for attr in 0 1 2 4 5 7; do
-            #Print the result
-            echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
-        done
-        echo #Newline
-    done
-done
+pad_to_centre() {
+    declare width="$1"
+    declare char="$2"
+    declare text="$3"
 
-exit 0
+    declare padding
+    padding="$(printf '%0.1s' "${char}"{1..500})"
 
-# TODO: I'd like to have a param to show more basic output, like this
+    printf '%*.*s %s %*.*s\n' 0 "$(((width - 2 - ${#text}) / 2))" "$padding" "$text" 0 "$(((width - 1 - ${#text}) / 2))" "$padding"
+}
 
-#!/bin/bash
-#
-#   This file echoes a bunch of color codes to the
-#   terminal to demonstrate what's available.  Each
-#   line is the color code of one forground color,
-#   out of 17 (default + 16 escapes), followed by a
-#   test use of that color on all nine background
-#   colors (default + 8 escapes).
-#
+# colour count
+declare count
+count="$(tput colors)"
 
-T='gYw' # The test text
+# loop offsets
+declare offset=0
+declare maxOffset=36
+while ((offset < maxOffset)); do
+    # loop index
+    declare i="$offset"
+    while ((i < count)); do
+        # print color
+        tput setaf "$i"
+        tput setab "$i"
+        echo -n "|$(pad_to_centre '10' ' ' "$i")|"
+        tput sgr0
 
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m"
-
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-    '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-    '  36m' '1;36m' '  37m' '1;37m'; do
-    FG=${FGs// /}
-    echo -en " $FGs \033[$FG  $T  "
-    for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
-        echo -en "\033[$FG\033[$BG  $T  \033[0m"
+        i=$((i + maxOffset))
     done
     echo
+
+    offset=$((offset + 1))
 done
-echo
