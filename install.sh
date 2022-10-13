e822835cfc26fcabcc7580f0e1942b28ffe8d386
William Villeneuve <ciiqr@users.noreply.github.com>
2022-10-12 20:12:48 -0400

    organized states a bit, added plugins for a few state types, updated install.sh script for nk, removed more unused salt configs

--- install.sh
+++ install.sh
@@ -2,27 +2,15 @@
 
 set -e
 
-# TODO: as needed
-# declare script_dir
-# script_dir="$(dirname "$(readlink -f ~/.local/bin/macro)")"
+declare script_dir
+script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-# TODO: configure nix
-# NIX_EXTRA_CONF='
-# experimental-features = nix-command flakes
-# warn-dirty = false
-# '
-# TODO: OR: ~/.config/nix/nix.conf
+# install nk
+curl -fsSL https://raw.githubusercontent.com/ciiqr/nk/HEAD/install.sh | bash
 
-# nix
-if type nix >/dev/null 2>&1; then
-    # update
-    if [[ "$OSTYPE" == darwin* ]]; then
-        sudo -i sh -c 'nix-channel --update && nix-env -iA nixpkgs.nix && launchctl remove org.nixos.nix-daemon && launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist'
-    fi
-else
-    # install
-    # TODO: consider: --no-modify-profile
-    sh <(curl -L https://nixos.org/nix/install) --daemon
-fi
+# add to path
+export PATH="${PATH}:${HOME}/.nk/bin"
 
-# TODO: nix-darwin OR home-manager...
+# provision
+cd "$script_dir"
+nk provision
