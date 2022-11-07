29ac589db79880695364bdbae0c5af21e558f596
William Villeneuve <ciiqr@users.noreply.github.com>
2022-11-07 18:13:21 -0500

    fix auto install inside codespaces

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ curl -fsSL 'https://raw.githubusercontent.com/ciiqr/nk/HEAD/install.sh' | bash
 # add to path
 export PATH="${PATH}:${HOME}/.nk/bin"
 
+# TODO: remove once we support remote plugins?
+# shellcheck disable=SC2154
+if [[ ! -d ../nk-plugins && "$CODESPACES" == 'true' ]]; then
+    echo '==> clone nk-plugins'
+    git clone https://github.com/ciiqr/nk-plugins.git ../nk-plugins
+fi
+
 echo '==> provision'
 
 # provision
