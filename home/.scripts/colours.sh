d721ff6349eeec60a68a916f61d53923a27d7bd5
William Villeneuve <ciiqr@users.noreply.github.com>
2022-12-03 16:28:23 -0500

    added 24-bit colour test to colour script

--- home/.scripts/colours.sh
+++ home/.scripts/colours.sh
@@ -124,6 +124,24 @@ print_set_4() {
     done
 }
 
+print_set_24-bit() {
+    echo 'will render a rainbow gradient below (if 24 bit colours supported):'
+    awk 'BEGIN{
+        s="          "; s=s s s s s s s s;
+        for (colnum = 0; colnum<77; colnum++) {
+            r = 255-(colnum*255/76);
+            g = (colnum*510/76);
+            b = (colnum*255/76);
+            if (g>255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum+1,1);
+        }
+        printf "\n";
+    }'
+
+}
+
 declare set_index="${1:-1}"
 
 "print_set_${set_index}"
