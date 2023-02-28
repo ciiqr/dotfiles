24c127266529f2c0b889859eb9294b8dd57e9c7f
William Villeneuve <ciiqr@users.noreply.github.com>
2023-02-28 09:55:59 -0500

    windows support

---
+++ windows/home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# no header
+cls
+
+# aliases
+function nk() {
+    cargo run --manifest-path $HOME\Projects\nk\Cargo.toml -q -- @args
+}
+
+# omp
+if (Get-Command "oh-my-posh" -ErrorAction "SilentlyContinue") {
+    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\probua.minimal.omp.json" | Invoke-Expression
+}
