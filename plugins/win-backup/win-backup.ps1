32dc1e8ec15f4cf66c78b4338e6cfef1311735aa
William Villeneuve <ciiqr@users.noreply.github.com>
2023-06-27 22:36:20 -0400

    windows: moved backup script into a plugin

---
+++ plugins/win-backup/win-backup.ps1
@@ -0,0 +1,76 @@
+Param (
+    [Parameter(Mandatory)]
+    [ValidateSet("provision")]
+    [string]$command,
+
+    [Parameter(Mandatory)]
+    [string]$info_json,
+
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [string]$stdin
+)
+
+# stop on first error
+$ErrorActionPreference = "Stop"
+
+function backup() {
+    Param (
+        [Parameter(Mandatory)]
+        [string]$backupDir
+    )
+
+    # make backup directory
+    [void](mkdir -Force "$backupDir")
+
+    # powershell history
+    copy-item "$((Get-PSReadlineOption).HistorySavePath)" "${backupDir}\powershell-history.txt"
+
+    # ~/Documents
+    robocopy "${env:USERPROFILE}\Documents" "${backupDir}\Documents" /mir /mt /NJH
+
+    # wow Interface\Addons
+    robocopy '\Program Files (x86)\World of Warcraft\_retail_\Interface\Addons' "${backupDir}\wow\_retail_\Interface\Addons" /mir /mt /NJH
+
+    # wow WTF
+    robocopy '\Program Files (x86)\World of Warcraft\_retail_\WTF' "${backupDir}\wow\_retail_\WTF" /mir /mt /NJH
+}
+
+function backup_provision() {
+    $states = ConvertFrom-Json $stdin
+
+    $baseBackupDir = "${env:USERPROFILE}\.backup"
+    if (-not (Test-Path $baseBackupDir -PathType Container)) {
+        @{
+            status = "failed"
+            changed = $false
+            description = "backup"
+            output = "backup: backup directory missing: $baseBackupDir"
+        } | ConvertTo-Json -Compress
+        return
+    }
+
+    $result = @{
+        status = "success"
+        # TODO: update based on changes above
+        changed = $false
+        description = "backup"
+        output = ""
+    }
+
+    $result.output = (backup -backupDir "${$baseBackupDir}\$($env:COMPUTERNAME.ToLower())") -join "`n"
+    if (!$?) {
+        $result.status = "failed"
+    }
+
+    $result | ConvertTo-Json -Compress
+}
+
+switch ($command) {
+    "provision" {
+        backup_provision
+    }
+    default {
+        echo "backup: unrecognized subcommand: ${command}"
+        exit 1
+    }
+}
--- windows/home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,34 +5,8 @@ cls
 $ErrorActionPreference = "Stop"
 
 # aliases
-# function nk() {
-#     cargo run --manifest-path $HOME\Projects\nk\Cargo.toml -q -- @args
-# }
-
 function path() {
     $env:Path.split(";")
-}
-
-function backup() {
-    $backupDir = "${env:USERPROFILE}\.backup\$($env:COMPUTERNAME.ToLower())"
-
-    # make backup directory
-    [void](mkdir -Force "$backupDir")
-
-    # backup files
-    echo "==> backup files"
-
-    echo "- powershell history"
-    copy-item "$((Get-PSReadlineOption).HistorySavePath)" "${backupDir}\powershell-history.txt"
-
-    echo "- ~/Documents"
-    robocopy "${env:USERPROFILE}\Documents" "${backupDir}\Documents" /mir /mt /NJH
-
-    echo "- wow Interface\Addons"
-    robocopy '\Program Files (x86)\World of Warcraft\_retail_\Interface\Addons' "${backupDir}\wow\_retail_\Interface\Addons" /mir /mt /NJH
-
-    echo "- wow WTF"
-    robocopy '\Program Files (x86)\World of Warcraft\_retail_\WTF' "${backupDir}\wow\_retail_\WTF" /mir /mt /NJH
 }
 
 # omp
