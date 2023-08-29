e226dbd1708efb01a0159df4b1f9f9e4a643d13d
William Villeneuve <ciiqr@users.noreply.github.com>
2023-08-29 10:54:14 -0400

    windows: nk shim so I don't have to cd every time

--- windows/home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,11 @@ function path() {
     $env:Path.split(";")
 }
 
+function nk() {
+    cd C:\Users\william\Projects\dotfiles
+    nk.exe @args
+}
+
 # omp
 if (Get-Command "oh-my-posh" -ErrorAction "SilentlyContinue") {
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\probua.minimal.omp.json" | Invoke-Expression
