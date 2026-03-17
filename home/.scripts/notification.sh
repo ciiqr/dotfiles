0043594c89d456e12b75eab49b655965f4940ca7
William Villeneuve <ciiqr@users.noreply.github.com>
2026-03-17 10:10:12 -0400

    back on arch

--- home/.scripts/notification.sh
+++ home/.scripts/notification.sh
@@ -1,34 +1,68 @@
 #!/usr/bin/env bash
 
-declare subcommand="$1"
-declare title="$2"
-declare message="$3"
-# TODO: fix up with proper options
-# TODO: maybe keep that common part in a base/frontend notification.sh & have some library files per platform
-# TODO: may need to switch out osascript with a proper app with timeouts (or at least wait till dismissed) options
-# declare opt="$4"
-# declare time="$5"
+if [[ "$OSTYPE" == 'darwin'* ]]; then
+    declare subcommand="$1"
+    declare title="$2"
+    declare message="$3"
+    # TODO: fix up with proper options
+    # TODO: maybe keep that common part in a base/frontend notification.sh & have some library files per platform
+    # TODO: may need to switch out osascript with a proper app with timeouts (or at least wait till dismissed) options
+    # declare opt="$4"
+    # declare time="$5"
 
-if [[ "$#" == 0 ]]; then
-    echo 'usage: '
-    echo '  ~/.scripts/notification.sh send <title> <message> [-t time]'
-    exit 1
-fi
+    if [[ "$#" == 0 ]]; then
+        echo 'usage: '
+        echo '  ~/.scripts/notification.sh send <title> <message> [-t time]'
+        exit 1
+    fi
 
-if [[ "$subcommand" == 'send' ]]; then
-    osascript - "$title" "$message" <<<'
-        on run argv
-            set title to item 1 of argv
-            set message to item 2 of argv
-            display notification message with title title
-        end run
-    '
+    if [[ "$subcommand" == 'send' ]]; then
+        osascript - "$title" "$message" <<<'
+            on run argv
+                set title to item 1 of argv
+                set message to item 2 of argv
+                display notification message with title title
+            end run
+        '
+    else
+        echo "invalid subcommand: ${subcommand}"
+        exit 1
+    fi
+
+    # TODO: would be cool to support sounds (custom or otherwise)
+    # ie. we could have these play for the healthcheck command
+    # osascript -e 'display notification "Hero" with title "Test sound" sound name "Hero"'
+    # osascript -e 'display notification "Glass" with title "Test sound" sound name "Glass"'
+
 else
-    echo "invalid subcommand: ${subcommand}"
-    exit 1
-fi
+    declare subcommand="$1"
+    declare title="$2"
+    declare message="$3"
+    # TODO: fix up with proper options
+    # TODO: maybe keep that common part in a base/frontend notification.sh & have some library files per platform
+    declare opt="$4"
+    declare time="$5"
 
-# TODO: would be cool to support sounds (custom or otherwise)
-# ie. we could have these play for the healthcheck command
-# osascript -e 'display notification "Hero" with title "Test sound" sound name "Hero"'
-# osascript -e 'display notification "Glass" with title "Test sound" sound name "Glass"'
+    if [[ "$#" == 0 ]]; then
+        echo 'usage: '
+        echo '  ~/.scripts/notification.sh send <title> <message> [-t time]'
+        exit 1
+    fi
+
+    if [[ "$subcommand" == 'send' ]]; then
+        declare -a args=()
+        if [[ -n "$title" ]]; then
+            args+=("$title")
+        fi
+        if [[ "$opt" == '-t' ]]; then
+            args+=('-t' "$time")
+        fi
+        args+=("$message")
+
+        notify-send "${args[@]}"
+    else
+        echo "invalid subcommand: ${subcommand}"
+        exit 1
+    fi
+
+fi
